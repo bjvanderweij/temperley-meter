f22e9cc0e4b3f19b91f6e60b4fd076249ccca55f
Bastiaan van der Weij <bjvanderweij@gmail.com>
2019-05-10 18:00:45 +0200

    isolate performance model from meter model

--- meter.py
+++ meter.py
@@ -36,8 +36,7 @@ class BeatLocation(Regularity):
 
     def __init__(self, tactus_interval, subdivision, beat, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        beat_length = tactus_interval // subdivision
-        idealised = beat_length * beat
+        idealised = beat * tactus_interval // subdivision
         symbols = self.symbols
         parameters = np.asarray([self.probability(s) for s in symbols])
 
@@ -76,22 +75,14 @@ class TactusInterval(Conditional):
 
 class MeterModel:
 
-    def __init__(self, min_tactus=9, max_tactus=23, beat_span=3, subdivisions=[[2,3], [2,3]]):
+    def __init__(self, subdivisions=[[2,3], [2,3]]):
         """Initialize a generative meter model.
 
         Keyword arguments:
-        min_tactus -- integer specifying minimum tactus interval (default 9)
-        min_tactus -- maximum tactus interval (default 23)
-        beat_span -- number of pips before and after ideal beat location to consider (default 3)
         subdivisions -- list of lists where each list specifies the subdivision possibilities for 
             the corresponding level (default [[2,3],[2,3]])
         """
-        # Generative parameters
-        self.beat_span = beat_span
-        self.min_tactus = min_tactus
-        self.max_tactus = max_tactus
         # Probability distributions
-        self.tactus_interval = TactusInterval(min_tactus, max_tactus)
         self.subdivisions = subdivisions
         self.metrical_levels = len(subdivisions) 
         self.levels=[Bernouilli(.78, symbols=self.subdivisions[0]), Bernouilli(.76, symbols=self.subdivisions[1])]
@@ -105,7 +96,6 @@ class MeterModel:
             [0, 1, 2, 3],
             [Bernouilli(.001), Bernouilli(.38), Bernouilli(.74), Bernouilli(.95)],
         )
-        self.first_tactus=FirstTactus(min_tactus, max_tactus)
         self.note_first_tactus=Bernouilli(.6)
         self.another_beat=Bernouilli(.95)
 
@@ -116,6 +106,33 @@ class MeterModel:
             for phase in range(grouping):
                 results.append(((subdivision, grouping), phase))
         return results
+
+    def meter_probability(self, levels, phases):
+        assert len(levels) - 1 == len(phases) 
+        meter = [d.probability(l) for d, l in zip(self.levels, levels)]
+        phase = [cd.conditional_probability(m, phase) for cd, m, phase in zip(self.phase, levels[1:], phases)]
+        return reduce(float.__mul__, meter + phase)
+
+class PerformanceModel(MeterModel):
+
+    def __init__(self, *args, min_tactus=9, max_tactus=23, beat_span=3, **kwargs):
+        """Initialize a generative meter model.
+
+        Keyword arguments:
+        min_tactus -- integer specifying minimum tactus interval (default 9)
+        min_tactus -- maximum tactus interval (default 23)
+        beat_span -- number of pips before and after ideal beat location to consider (default 3)
+        subdivisions -- list of lists where each list specifies the subdivision possibilities for 
+            the corresponding level (default [[2,3],[2,3]])
+        """
+        super().__init__(*args, **kwargs)
+        # Generative parameters
+        self.beat_span = beat_span
+        self.min_tactus = min_tactus
+        self.max_tactus = max_tactus
+        # Probability distributions
+        self.tactus_interval = TactusInterval(min_tactus, max_tactus)
+        self.first_tactus=FirstTactus(min_tactus, max_tactus)
 
     @property
     def tactus_intervals(self):
@@ -143,16 +160,9 @@ class MeterModel:
 
     def beat_probability(self, salience, onset):
         return self.note.conditional_probability(salience, onset)
-
-    def meter_probability(self, levels, phases):
-        assert len(levels) - 1 == len(phases) 
-        meter = [d.probability(l) for d, l in zip(self.levels, levels)]
-        phase = [cd.conditional_probability(m, phase) for cd, m, phase in zip(self.phase, levels[1:], phases)]
-        return reduce(float.__mul__, meter + phase)
                 
 def argmax_index(parameters, f):
     return max(range(len(parameters)), key=lambda i: f(parameters[i]))
-
 
 # Not that fast because entire rhythm is passed and hashed?
 @memoized
@@ -166,7 +176,6 @@ def interval_probability(model, excerpt, beats):
     return sum(probabilities)
 
 def generate_tactus_table(model, rhythm):
-
     print('generating tactus analyses.')
     probabilities, analyses = {}, {}
 
@@ -183,13 +192,12 @@ def generate_tactus_table(model, rhythm):
             beat_dists = [model.beat_location(interval, subdivision, b) for b in range(1, subdivision)]
             # All possible sequences of beat locations
             beat_locations = list(cartesian_product(*(d.symbols for d in beat_dists)))
-            # PROBS
             beats_prob_f = lambda beats: sum([np.log(d.probability(b)) for b, d in zip(beats, beat_dists)])
             probability_f = lambda beats: beats_prob_f(beats) + interval_probability(model, exerpt, beats)
+            # Find and store the best analysis
             i =  argmax_index(beat_locations, probability_f)
             analyses[subdivision, begin, end] = beat_locations[i]
             probabilities[subdivision, begin, end] = probability_f(beat_locations[i])
-            #print(probabilities[subdivision, begin, end])
 
     return probabilities, analyses
 
@@ -198,11 +206,13 @@ def generate_meter_table(model, rhythm, lower_level_probabilities):
     print('generating table.')
 
     probabilities, analyses = {}, {}
-    onset = lambda pip: (rhythm[pip] if pip >= 0 and pip < len(rhythm) else False)
+    onset_f = lambda pip: (rhythm[pip] if pip >= 0 and pip < len(rhythm) else False)
     meters = model.meters 
     # Mass?
     for begin, end, first_beat, last_beat in model.tactus_spans(len(rhythm)):
         interval = end - begin
+        onset_at_end = onset_f(end)
+        onset_at_begin = onset_f(begin)
         for (subdivision, grouping), phase in meters:
             #print('[%d to %d)%s%s\t%s' % (begin, end, ('^' if first_beat else ''), ('$' if last_beat else ''), (phase, subdivision, grouping)))
             # <phase> is interpreted to correspond to the phase at the <end>
@@ -216,22 +226,26 @@ def generate_meter_table(model, rhythm, lower_level_probabilities):
                 a hypothetical previous interval.
                 """
                 return (
-                    # Probability of best analysis of current interval (independent of grouping and phase)
+                    # Probability of best lower-level analysis of current interval 
+                    # (this is independent of grouping and phase)
                     lower_level_probabilities[subdivision, begin, end] +
-                    # Probability of of onset at current tactus beat
-                    np.log(model.beat_probability(3 if strong else 2, onset(end))) +
+                    # Probability of onset at current tactus beat
+                    np.log(model.beat_probability(3 if strong else 2, onset_at_end)) +
                     (
                         # Prior probability of the meter and phase
                         np.log(model.meter_probability([subdivision, grouping], [phase])) +
                         # Probability of a note on the first tactus beat
-                        np.log(model.note_first_tactus.probability(onset(begin))) +
+                        np.log(model.note_first_tactus.probability(onset_at_begin)) +
                         # Probability of first tactus interval duration
                         np.log(model.first_tactus.probability(interval))
                         # Theoretically, below would make sense
-                        # + model.beat_probability(3 if strong else 2, onset(end)) +
+                        # + model.beat_probability(3 if previous_phase == 0 else 2, onset(end)) +
                         if first_beat else 
+                        # Probability of best analysis previous tactus interval
                         probabilities[subdivision, grouping, previous_phase, begin-previous_interval, begin] +
+                        # Probability of tactus interval given previous tactus interval
                         np.log(model.tactus_interval.conditional_probability(previous_interval, interval)) + 
+                        # Probability of generating another beat or not
                         np.log(model.another_beat.probability(last_beat))
                     )
                 )
@@ -269,7 +283,7 @@ def trace_back(analyses, tactus_analyses, subdivision, grouping, phase, begin, e
             subtactus_beat_locations=[subtactus_beats] + subtactus_beat_locations
         )
     else:
-        return tactus_beats, subtactus_beat_locations, (end, phase, subdivision, grouping)
+        return tactus_beats, subtactus_beat_locations, (subdivision, grouping, phase, end)
 
 def almost_equal(p1, p2, eta=1e-10):
     return abs(p1 - p2) < eta
@@ -281,7 +295,7 @@ def run():
     parser.add_argument('--note_file')
     args = parser.parse_args()
 
-    model = MeterModel()
+    model = PerformanceModel()
 
     two_two = .78 * .76
     assert model.meter_probability([2, 2], [0]) == two_two * .65
@@ -299,7 +313,7 @@ def run():
     #    plt.plot(x, list(map(lambda i: beat_location.probability(i), x)))
     #plt.show()
 
-    model = MeterModel()
+    model = PerformanceModel()
     #rhythm = [True, False, True]
     if args.note_file is None:
         return 
@@ -311,26 +325,51 @@ def run():
     tactus_probabilities, tactus_analyses = generate_tactus_table(model, rhythm)
     probabilities, analyses = generate_meter_table(model, rhythm, tactus_probabilities)
     meter, ll = best_analysis(model, rhythm, probabilities)
-    tactus_beats, subtactus_beat_locations, (first_beat, phase, subdivision, grouping) = trace_back(analyses, tactus_analyses, *meter)
+    tactus_beats, subtactus_beat_locations, (subdivision, grouping, phase, first_beat) = trace_back(analyses, tactus_analyses, *meter)
     tb = [first_beat] + tactus_beats
     print(tb)
     print([b - a for a, b in zip(tb[:-1], tb[1:])])
     print(subtactus_beat_locations)
     print('phase %s, subdivision %s, grouping %s, likelihood %s' % (phase, subdivision, grouping, ll))
+    print()
+    print_analysis(rhythm, grouping, first_beat, tactus_beats, subtactus_beat_locations, phase)
 
 def chunks(l, n):
-    """Yield successive n-sized chunks from l."""
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
+    count = 0
+    buf = []
+    for i in l:
+        buf += [i]
+        count += 1
+        if count == n:
+            yield buf
+            count = 0
+            buf = []
+    if count > 0:
+        yield buf
 
-def print_analysis(rhythm, meter):
-    line_width = 80
-    for chunk in chunks(enumerate(rhythm), line_width):
-        (phase, onset), *rest = chunk
-        l3 = '' 
-        l2 = ''
-        l1 = ''
-        r = ''
+def print_analysis(rhythm, grouping, first_beat, tactus_beats, subbeats, phase):
+    line_width = 60
+    grid = [False, ] * -first_beat + list(rhythm)
+    l1_grid = [False, ] * len(grid)
+    l2_grid = [False, ] * len(grid)
+    l3_grid = [False, ] * len(grid)
+    for beat_location, subbeats in zip([first_beat] + tactus_beats, subbeats):
+        beat_location -= first_beat
+        l2_grid[beat_location] = True
+        # Every l2 beat is also a l1 beat (thanks Ray and Fred!)
+        l1_grid[beat_location] = True
+        for b in subbeats:
+            l1_grid[beat_location + b] = True
+        if phase == 0:
+            l3_grid[beat_location] = True
+        phase = (phase + 1) % grouping
+        
+    for chunk in chunks(list(enumerate(grid)), line_width):
+        # Print an 'o' if 
+        l1 = ''.join('o' if l1_grid[pip] else ' ' for pip, onset in chunk)
+        l2 = ''.join('o' if l2_grid[pip] else ' ' for pip, onset in chunk)
+        l3 = ''.join('o' if l3_grid[pip] else ' ' for pip, onset in chunk)
+        r = ''.join('x' if onset else '-' for pip, onset in chunk)
         print('%s\n%s\n%s\n\n%s\n\n' % (l3, l2, l1, r))
 
 if __name__ == '__main__':
